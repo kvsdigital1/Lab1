7cce6ebe8bdec070d65dbcc86b1f98885057c0a4
kvsdigital1 <84262768+kvsdigital1@users.noreply.github.com>
2021-05-16 18:51:30 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create function drawRectangle() to draw a rectangle. 
+// The rectangle should start from coordinate (10,10) with a height of 100 and width of 150
+
+drawRectangle();
+
+function drawRectangle(){
+  
+}
